aa1a7ba3927d0ebeda89b72e0799743a7f64f2ec
Zachary Klima <zklima@hawk.iit.edu>
2015-04-12 04:30:03 -0500

    Added the other end of the association, and added a validates statement to ensure that the email username is alphanumeric + underscore.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,6 @@
 class Email < ActiveRecord::Base
+  has_many :accounts
+
+  validates_format_of :e_username, :with => /^[A-Za-z\d_]+$/
+  #Dug that up from archive.railsforum.org/viewtopic.php?id=13211
 end
