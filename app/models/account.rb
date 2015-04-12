0372e7555fe258e1c1cd451c903f18d1c08b7693
Zachary Klima <zklima@hawk.iit.edu>
2015-04-12 04:46:36 -0500

    Added presence: true validations to all fields and uniqueness: true where necessary.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,12 @@
 class Account < ActiveRecord::Base
   belongs_to :email
+
+  validates :a_service, presence:true
+  #Validation may be entirely different for different accounts, so it would be silly
+  #to restrict to alphanumeric when a username might allow UTF-8 on a certain site.
+  #
+  #Password and host do not require super-serious validation, only that they aren't blank (and for username, that is is unique).
+  validates :a_username, presence: true
+  validates :a_username, uniqueness: true
+  validates :a_pass, presence: true
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,11 @@
 class Email < ActiveRecord::Base
   has_many :accounts
 
+  validates :e_username, presence: true
+  validates :e_username, uniqueness: true
   validates_format_of :e_username, :with => /^[A-Za-z\d_]+$/
   #Dug that up from archive.railsforum.org/viewtopic.php?id=13211
+
+  validates :e_service, presence: true
+  validates :e_pass, presence: true
 end
