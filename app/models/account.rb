82dc9527c5fc10847a9551896f2b8f5aeaa9a22e
Zachary Klima <zklima@hawk.iit.edu>
2015-04-12 05:35:30 -0500

    Fixed an issue where a user could not use the same account on different sites. Comments are available.

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,7 +6,10 @@ class Account < ActiveRecord::Base
   #to restrict to alphanumeric when a username might allow UTF-8 on a certain site.
   #
   #Password and host do not require super-serious validation, only that they aren't blank (and for username, that is is unique).
+
   validates :a_username, presence: true
-  validates :a_username, uniqueness: true
+  #validates :a_username, uniqueness: true
+  #But what if I want to name myself "Jimbobbillyjoe" on multiple sites? I can't use the above code. Somehow I need validates to only
+  #throw an error if uniqueness of username is false AND if uniqueness of service is false.
   validates :a_pass, presence: true
 end
