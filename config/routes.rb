024d8ae0b6c3175d1e8f56ea8228bf08eb56df1a
Zachary Klima <zklima@hawk.iit.edu>
2015-04-12 05:09:35 -0500

    Changed the root path to the somewhat-default "welcome#index".

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,18 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
+  
+  root 'welcome#index'
+end
+
+
+
+
+
+
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +66,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
