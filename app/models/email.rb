6a027287a96a8b17252856df34440558ac60ae44
Zachary Klima <zklima@hawk.iit.edu>
2015-04-12 15:22:43 -0500

    Commented out the validation statement, which was apparently a security risk. Also changed the url from .org to .com so that one can actually follow it.

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,8 +3,8 @@ class Email < ActiveRecord::Base
 
   validates :e_username, presence: true
   validates :e_username, uniqueness: true
-  validates_format_of :e_username, :with => /^[A-Za-z\d_]+$/
-  #Dug that up from archive.railsforum.org/viewtopic.php?id=13211
+  #validates_format_of :e_username, :with => /^[A-Za-z\d_]+$/
+  #Dug that up from archive.railsforum.com/viewtopic.php?id=13211
 
   validates :e_service, presence: true
   validates :e_pass, presence: true
