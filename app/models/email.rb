01f006b38243466646d3c4489356e4ad2e45cd6d
Zachary Klima <zklima@hawk.iit.edu>
2015-04-12 23:23:01 -0500

    Changed the validation requirements on email.e_username; however, because an email object currently cannot be established, there is no way to test whether or not this particular blurb works.

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,7 +3,7 @@ class Email < ActiveRecord::Base
 
   validates :e_username, presence: true
   validates :e_username, uniqueness: true
-  #validates_format_of :e_username, :with => /^[A-Za-z\d_]+$/
+  validates_format_of :e_username, :with => [A-Za-z0-9]
   #Dug that up from archive.railsforum.com/viewtopic.php?id=13211
 
   validates :e_service, presence: true
